c9f021d1f4c0fe4dd405977512b9311d69bff3de
BleeesJc <matevalen.xd@gmail.com>
2024-11-27 10:39:03 -0400

    Ejecucion directa

--- Scripts/LecturaDeDatos.py
+++ Scripts/LecturaDeDatos.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Configuración de la base de datos
 db_config = {
-    'host': 'localhost',
+    'host': '192.168.0.26',
     'user': 'root',
     'password': '',
     'database': 'puentesdb'
