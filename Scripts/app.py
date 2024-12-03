3f3fbf12b0568a10fd6d615cf6a25f19351c0ed0
Nanda <fmattosteran10@gmail.com>
2024-12-03 11:35:31 -0400

    home listo falta redireccion en app.py

--- Scripts/app.py
+++ Scripts/app.py
@@ -1,23 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from ABMServidor import abm_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Registrar Blueprints con prefijos
 app.register_blueprint(abm_bp, url_prefix="/abm")
 
-
 @app.route("/")
 def index():
-    return render_template("home.html")
+    # Redirige a la ruta "/abm"
+    return redirect(url_for("abm"))
 
 @app.route("/abm")
 def abm():
     return render_template("abm.html")
-
-@app.route("/prinicpal")
-def principal():
-    return render_template("principal.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
